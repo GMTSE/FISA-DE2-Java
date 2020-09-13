a61cacee57852b2826851a6ed4953ba14276796b
GMTSE <guillaume@telecom-st-etienne.fr>
2020-09-13 12:41:06 +0200

    no mounting ro

--- mount_covid.sh
+++ mount_covid.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bwrap --bind / / --dev /dev `#--ro-bind` --bind /home/gmuller/2020_TSE/TSE/Cours/COVID-slides/ /home/gmuller/2020_TSE/TSE/Cours/FISA/DE2/Java/Cours/GM/COVID-slides/ bash
+bwrap --bind / / --dev /dev `#--ro-bind` --bind /home/gmuller/2020_TSE/TSE/Cours/INTRO-slides/ /home/gmuller/2020_TSE/TSE/Cours/FISA/DE2/Java/Cours/GM/INTRO-slides/ bash
